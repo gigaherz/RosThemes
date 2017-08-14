962caffa1a28a7185d688eca7d61ccb314d0a587
gigaherz <gigaherz@gmail.com>
2017-08-14 23:28:25 +0200

    Code import.

---
+++ ThemePacker/Program.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+
+namespace ThemePacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bitmapPath = null;
+            string outputFile = ".";
+            int values = 0;
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("-") || arg.StartsWith("/"))
+                {
+                    // TODO
+                    Console.WriteLine($"Unrecognized option: {arg}");
+                    Console.WriteLine();
+                    DisplayHelp();
+                    return;
+                }
+
+                values++;
+                switch (values)
+                {
+                    case 1: bitmapPath = arg; break;
+                    case 2: outputFile = arg; break;
+                }
+            }
+
+            if (values < 1)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            var entries = Scan(bitmapPath);
+            RenderAndSave(entries, outputFile);
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine($"Command Line: {AppDomain.CurrentDomain.FriendlyName} <bitmap folder> [<pack file>] [<options>]");
+            Console.WriteLine("Options:");
+            Console.WriteLine("\tNothing here yet.");
+        }
+
+        public static List<Entry> Scan(string path)
+        {
+            var entries = new List<Entry>();
+
+            var dir = new DirectoryInfo(path);
+            foreach (var file in dir.EnumerateFiles())
+            {
+                if (file.Extension.ToLowerInvariant() != ".bmp")
+                    continue;
+
+                Bitmap src = new Bitmap(file.FullName);
+
+                Debug.WriteLine($"{file.FullName}: {src.Width}x{src.Height} {src.PixelFormat}");
+
+                var name = Path.GetFileNameWithoutExtension(file.Name);
+
+                if (name.IndexOf('_') >= 0)
+                    name = name.Substring(name.IndexOf('_') + 1);
+
+                name = name.Replace("RADIOBUTTON", "[Rb]");
+                name = name.Replace("CHECKBOX", "[Cb]");
+                name = name.Replace("HORIZONTAL", "[H]");
+                name = name.Replace("VERTICAL", "[V]");
+                name = name.Replace("BUTTON", "[Btn]");
+                name = name.Replace("EXPLORER", "[Xp]");
+                name = name.Replace("TOOLBAR", "[Tb]");
+                name = name.Replace("TASKBAR", "[Tkb]");
+                name = name.Replace("START", "[S]");
+                name = name.Replace("BACKGROUND", "[Bg]");
+
+                entries.Add(new Entry
+                {
+                    FileName = file.Name,
+                    Label = name,
+                    Image = src
+                });
+            }
+
+            entries.Sort((a, b) => string.CompareOrdinal(a.FileName, b.FileName));
+            for (var i = 0; i < entries.Count; i++)
+            {
+                var item = entries[i];
+                item.LineNumber = (i + 1).ToString();
+            }
+
+            return entries;
+        }
+
+        public static Size Canvas = new Size(1024,1024);
+
+        private static bool ProcessLayout(List<Entry> entries)
+        {
+            var freeAreas = new List<Rectangle?> { new Rectangle(5, 5, Canvas.Width-10, Canvas.Height-10) };
+
+#if false
+            var itemsSorting = new List<Entry>(entries);
+            itemsSorting.Sort((a, b) =>
+            {
+                var r = Math.Sign(b.W * b.H - a.W * a.H);
+                if (r != 0) return r;
+                var w = Math.Sign(b.W - a.W);
+                if (w != 0) return w;
+                var h = Math.Sign(b.H - a.H);
+                if (h != 0) return h;
+                return 0;
+            });
+            var items = new Queue<Entry>(itemsSorting);
+#else
+            var items = new Queue<Entry>(entries);
+#endif
+
+            int processed = 0;
+            while (items.Count > 0)
+            {
+                processed++;
+
+                var item = items.Dequeue();
+
+                var iw = item.W;
+                var ih = item.H;
+
+                Rectangle? best = null;
+                var direction = 0;
+                var diff = int.MaxValue;
+                var diffOther = int.MaxValue;
+                foreach (var rect in freeAreas)
+                {
+                    var w = rect.Value.Width;
+                    var h = rect.Value.Height;
+
+                    if (iw > w || ih > h)
+                        continue;
+
+                    var dw = w - iw;
+                    var dh = h - ih;
+                    if (dw < dh)
+                    {
+                        if (dw < diff || (Math.Abs(dw - diff) < float.Epsilon && dh < diffOther))
+                        {
+                            best = rect;
+                            diff = dw;
+                            diffOther = dh;
+                            direction = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (dh < diff || (Math.Abs(dh - diff) < float.Epsilon && dw < diffOther))
+                        {
+                            best = rect;
+                            diff = dh;
+                            diffOther = dw;
+                            direction = 2;
+                        }
+                    }
+                }
+
+                if (!best.HasValue)
+                {
+                    Console.WriteLine("ERROR: I wasn't able to find a rectangle to put this in!");
+                    return false;
+                }
+                else
+                {
+                    var chosen = best.Value;
+                    freeAreas.Remove(best);
+
+                    item.X = chosen.X;
+                    item.Y = chosen.Y;
+
+                    if (direction == 1)
+                    {
+                        var t1 = diff * chosen.Height;
+                        var t2 = chosen.Width * diffOther;
+
+                        int merge = 0;
+                        if (t1 > t2) merge = 1;
+                        else merge = 2;
+
+                        if (merge == 1)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y,diff, chosen.Height));
+                        }
+                        else if (diff > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y,diff, ih));
+                        }
+
+                        if (merge == 2)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X, chosen.Y + ih,chosen.Width, diffOther));
+                        }
+                        else if (diffOther > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X, chosen.Y + ih,iw, diffOther));
+                        }
+
+                        if (merge == 0 && diff > 0 && diffOther > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y + ih,diff, diffOther));
+                        }
+                    }
+                    else
+                    {
+                        var t1 = diffOther * chosen.Height;
+                        var t2 = chosen.Width * diff;
+
+                        int merge = 0;
+                        if (t1 > t2) merge = 1;
+                        else merge = 2;
+
+                        if (merge == 1)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y,diffOther, chosen.Height));
+                        }
+                        else if (diffOther > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y,diffOther, ih));
+                        }
+                        if (merge == 2)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X, chosen.Y + ih,chosen.Width, diff));
+                        }
+                        else if (diff > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X, chosen.Y + ih,iw, diff));
+                        }
+                        if (merge == 0 && diff > 0 && diffOther > 0)
+                        {
+                            freeAreas.Add(new Rectangle(chosen.X + iw, chosen.Y + ih,diffOther, diff));
+                        }
+                    }
+                }
+
+                freeAreas.Sort((a, b) =>
+                {
+                    var a1 = a.Value.Width * a.Value.Height;
+                    var b1 = b.Value.Width * b.Value.Height;
+                    return Math.Sign(b1 - a1);
+                });
+            }
+
+            return true;
+        }
+
+        private static void RenderAndSave(List<Entry> entries, string outputFile)
+        {
+            int[] sizesToTry = { 1024, 1536, 2048, 3072, 4096 };
+            int wSize = 0;
+            int hSize = 0;
+
+            bool measured = false;
+            Bitmap target;
+            while (true)
+            {
+                target = new Bitmap(Canvas.Width, Canvas.Height, PixelFormat.Format32bppArgb);
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.CompositingQuality = CompositingQuality.AssumeLinear;
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.PixelOffsetMode = PixelOffsetMode.None;
+                    g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+                    g.SmoothingMode = SmoothingMode.None;
+
+                    using (var font = new Font(FontFamily.GenericSansSerif, 9))
+                    {
+                        if (!measured)
+                        {
+                            measured = true;
+                            foreach (var item in entries)
+                            {
+                                item.LabelSize = g.MeasureString(item.Label, font);
+                                item.LineNumberSize = g.MeasureString(item.LineNumber, font);
+
+                                if (item.Vertical)
+                                {
+                                    item.LabelSize = new SizeF(item.LabelSize.Height, item.LabelSize.Width);
+                                    item.W = (int)Math.Ceiling(Math.Max(item.RealW + item.LabelSize.Width, item.LineNumberSize.Width)) + 10;
+                                    item.H = (int)Math.Ceiling(Math.Max(item.RealH + item.LineNumberSize.Height, item.LabelSize.Height)) + 10;
+                                }
+                                else
+                                {
+                                    item.LineNumberSize = new SizeF(item.LineNumberSize.Height, item.LineNumberSize.Width);
+                                    item.W = (int)Math.Ceiling(Math.Max(item.RealW + item.LineNumberSize.Width, item.LabelSize.Width)) + 10;
+                                    item.H = (int)Math.Ceiling(Math.Max(item.RealH + item.LabelSize.Height, item.LineNumberSize.Height)) + 10;
+                                }
+
+                            }
+                        }
+
+                        if (!ProcessLayout(entries))
+                        {
+                            if (Canvas.Height > Canvas.Width)
+                                wSize++;
+                            else
+                                hSize++;
+                            Canvas = new Size(sizesToTry[wSize], sizesToTry[hSize]);
+
+                            target.Dispose();
+                            continue;
+                        }
+
+                        foreach (var item in entries)
+                        {
+                            g.DrawImage(item.Image, new Rectangle(item.X, item.Y, item.RealW, item.RealH), 0, 0, item.RealW, item.RealH, GraphicsUnit.Pixel);
+                            g.DrawString(item.Vertical ? item.LineNumber : item.Label, font, Brushes.Black, item.X, item.Y + item.RealH + 1);
+
+                            g.TranslateTransform(item.X + item.RealW + (item.Vertical?item.LabelSize:item.LineNumberSize).Width + 1, item.Y);
+                            g.RotateTransform(90);
+                            g.DrawString(item.Vertical ? item.Label : item.LineNumber, font, Brushes.Black, 0, 0);
+                            g.ResetTransform();
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            var bitmap = Path.Combine(Path.GetDirectoryName(outputFile), Path.GetFileNameWithoutExtension(outputFile) + ".png");
+            target.Save(bitmap);
+            File.WriteAllLines(outputFile, 
+                new[] {
+                    $"#Bitmap={bitmap}",
+                }.Concat(entries.Select(b => $"{b.FileName}={b.X},{b.Y},{b.RealW},{b.RealH},{b.Image.PixelFormat}")));
+        }
+    }
+
+    public class Entry
+    {
+        public string LineNumber { get; set; }
+        public string FileName { get; set; }
+        public string Label { get; set; }
+
+        public Bitmap Image { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int W { get; set; }
+        public int H { get; set; }
+
+        public int RealW => Image.Width;
+        public int RealH => Image.Height;
+
+        public bool Vertical => RealH >= RealW;
+
+        public SizeF LabelSize { get; set; }
+        public SizeF LineNumberSize { get; set; }
+
+        public override string ToString()
+        {
+            return $"({X},{Y},{W},{H})";
+        }
+    }
+}
---
+++ ThemeUnpacker/Program.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ThemeUnpacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metadataFile = null;
+            string outputFolder = ".";
+            int values = 0;
+            foreach(var arg in args)
+            {
+                if (arg.StartsWith("-") || arg.StartsWith("/"))
+                {
+                    // TODO
+                    Console.WriteLine($"Unrecognized option: {arg}");
+                    Console.WriteLine();
+                    DisplayHelp();
+                    return;
+                }
+
+                values++;
+                switch(values)
+                {
+                    case 1: metadataFile = arg; break;
+                    case 2: outputFolder = arg; break;
+                }
+            }
+
+            if (values < 1)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            Unpack(metadataFile, outputFolder);
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine($"Command Line: {AppDomain.CurrentDomain.FriendlyName} <pack file> [<output folder>] [<options>]" );
+            Console.WriteLine("Options:");
+            Console.WriteLine("\tNothing here yet.");
+        }
+
+        static void Unpack(string metadata, string output)
+        { 
+            if (!Directory.Exists(output))
+                Directory.CreateDirectory(output);
+
+#if false
+            string orig = null;
+#endif
+            Bitmap image = null;
+
+            try
+            {
+                var lines = File.ReadAllLines(metadata);
+                var regex = new Regex("^(?<filename>[^=]+)=(?<x>[0-9]+),(?<y>[0-9]+),(?<w>[0-9]+),(?<h>[0-9]+),(?<fmt>.*)$", RegexOptions.CultureInvariant);
+                var meta = new Regex("^#(?<prop>[^=]+)=(?<meta>.*)$", RegexOptions.CultureInvariant);
+                foreach (var ln in lines)
+                {
+                    string line = ln;
+                    int commentChar = line.IndexOf('#');
+                    if (commentChar == 0)
+                    {
+                        var metaMatch = meta.Match(line);
+                        if (metaMatch.Success)
+                        {
+                            var metaValue = metaMatch.Groups["meta"].Value;
+                            switch (metaMatch.Groups["prop"].Value)
+                            {
+#if false
+                                case "SourceFolder":
+                                    orig = metaValue;
+                                    break;
+#endif
+                                case "Bitmap":
+                                    if (image == null)
+                                        image = new Bitmap(new FileStream(metaValue, FileMode.Open, FileAccess.Read, FileShare.Read));
+                                    break;
+                            }
+                        }
+                        continue;
+                    }
+
+                    if (commentChar > 0)
+                        line = line.Substring(0, commentChar);
+
+                    line = line.Trim();
+
+                    if (line.Length == 0)
+                        continue;
+                    
+                    var match = regex.Match(line);
+                    var fileName = match.Groups["filename"].Value;
+                    if (!int.TryParse(match.Groups["x"].Value, out var x)) return;
+                    if (!int.TryParse(match.Groups["y"].Value, out var y)) return;
+                    if (!int.TryParse(match.Groups["w"].Value, out var w)) return;
+                    if (!int.TryParse(match.Groups["h"].Value, out var h)) return;
+                    if (!Enum.TryParse(match.Groups["fmt"].Value, true, out PixelFormat fmt)) return;
+
+#if false
+                    using (var bb = new Bitmap(new FileStream(Path.Combine(orig, fileName), FileMode.Open, FileAccess.Read, FileShare.Read)))
+                        SaveCustomBmp(Path.Combine(output, fileName), b, new Rectangle(x, y, w, h), fmt, bb.Palette);
+#else
+                    SaveCustomBmp(Path.Combine(output, fileName), image, new Rectangle(x, y, w, h), fmt, null);
+#endif
+                }
+            }
+            finally
+            {
+                image?.Dispose();
+            }
+        }
+
+        static int FindBestPixelFormat(Bitmap source, Rectangle rect, PixelFormat fmt, ColorPalette pal, out Color[] palette, out Color[,] pixels)
+        {
+            var colors = new HashSet<Color>(new ColorEquality());
+            pixels = new Color[rect.Width, rect.Height];
+
+            for (int y = 0; y < rect.Height; y++)
+            {
+                for (int x = 0; x < rect.Width; x++)
+                {
+                    var color = source.GetPixel(x + rect.Left, rect.Bottom - 1 - y);
+                    colors.Add(color);
+                    pixels[x, y] = color;
+                }
+            }
+
+            if (colors.Count > 256 || fmt.HasFlag(PixelFormat.Alpha))
+            {
+                palette = null;
+                return 32;
+            }
+
+            if (pal == null || colors.Count > pal.Entries.Length)
+                palette = colors.ToArray();
+            else
+                palette = pal.Entries.ToArray();
+
+            if (palette.Length > 16)
+                return 8;
+
+            if (palette.Length > 2)
+                return 4;
+
+            return 1;
+        }
+
+        static void SaveCustomBmp(string fileName, Bitmap source, Rectangle rect, PixelFormat fmt, ColorPalette pal)
+        {
+            var alpha = fmt.HasFlag(PixelFormat.Alpha);
+            if (alpha)
+                return;
+
+            var bitsPerPixel = FindBestPixelFormat(source, rect, fmt, pal, out var palette, out var pixels);
+
+            var bitmapData = new List<byte>();
+
+            // File Header
+            bitmapData.AddUShort(0x4D42);
+            bitmapData.AddUInt(0); // placeholder for the bmp file size in bytes
+            bitmapData.AddUShort(0); // reserved
+            bitmapData.AddUShort(0); // reserved
+            bitmapData.AddUInt(0); // placeholder for the pixel data start
+
+            // Info Header
+            bitmapData.AddUInt(40);
+            bitmapData.AddInt(rect.Width);
+            bitmapData.AddInt(rect.Height);
+            bitmapData.AddUShort(1);
+            bitmapData.AddUShort((ushort)bitsPerPixel);
+            bitmapData.AddUInt(0);
+            bitmapData.AddUInt(0); // can be 0 for uncompressed
+            bitmapData.AddInt(0x0B12);
+            bitmapData.AddInt(0x0B12);
+            if (palette != null)
+            {
+                var palLength = palette.Length == (1 << bitsPerPixel) ? 0 : palette.Length;
+                bitmapData.AddInt(palLength);
+                bitmapData.AddInt(palLength);
+                foreach (var color in palette)
+                {
+                    bitmapData.AddColor(color, alpha);
+                }
+            }
+            else
+            {
+                bitmapData.AddInt(0);
+                bitmapData.AddInt(0);
+            }
+
+            // Go back and set the bitmap data offset
+            bitmapData.SetInt(10, bitmapData.Count);
+
+            if (palette != null)
+            {
+                if (bitsPerPixel == 1)
+                {
+                    bool flush = (rect.Width & 3) != 0;
+                    for (int y = 0; y < rect.Height; y++)
+                    {
+                        int c = 0;
+                        int b = 0;
+                        for (int x = 0; x < rect.Width; x++)
+                        {
+                            var color = pixels[x, y];
+                            var index = Array.IndexOf(palette, color);
+                            switch (x & 7)
+                            {
+                                case 0: c = index; break;
+                                case 1: c = (c << 1) | index; break;
+                                case 2: c = (c << 1) | index; break;
+                                case 3: c = (c << 1) | index; break;
+                                case 4: c = (c << 1) | index; break;
+                                case 5: c = (c << 1) | index; break;
+                                case 6: c = (c << 1) | index; break;
+                                default:
+                                    c = (c << 1) | index;
+                                    bitmapData.Add((byte)c);
+                                    b++;
+                                    break;
+                            }
+                        }
+                        if (flush)
+                        {
+                            bitmapData.Add((byte)c);
+                            b++;
+                        }
+                        while ((b++ % 4) != 0)
+                            bitmapData.Add(0);
+                    }
+                }
+                else if (bitsPerPixel < 8)
+                {
+                    bool flush = (rect.Width & 1) != 0;
+                    for (int y = 0; y < rect.Height; y++)
+                    {
+                        int c = 0;
+                        int b = 0;
+                        for (int x = 0; x < rect.Width; x++)
+                        {
+                            var color = pixels[x, y];
+                            var index = Array.IndexOf(palette, color);
+                            if ((x & 1) == 0)
+                            {
+                                c = index << 4;
+                            }
+                            else
+                            {
+                                c = c | index;
+                                bitmapData.Add((byte)c);
+                                b++;
+                            }
+                        }
+                        if (flush)
+                        {
+                            bitmapData.Add((byte)c);
+                            b++;
+                        }
+                        while ((b++ % 4) != 0)
+                            bitmapData.Add(0);
+                    }
+                }
+                else
+                {
+                    for (int y = 0; y < rect.Height; y++)
+                    {
+                        int x;
+                        for (x = 0; x < rect.Width; x++)
+                        {
+                            var color = pixels[x, y];
+                            var index = Array.IndexOf(palette, color);
+                            bitmapData.Add((byte)index);
+                        }
+                        while ((x++ % 4) != 0)
+                            bitmapData.Add(0);
+                    }
+                }
+            }
+            else // assumes 32bit
+            {
+                for (int y = 0; y < rect.Height; y++)
+                {
+                    for (int x = 0; x < rect.Width; x++)
+                    {
+                        var color = pixels[x, y];
+                        bitmapData.AddColor(color, alpha);
+                    }
+                }
+            }
+
+            // Go back and set the bitmap data offset
+            bitmapData.SetInt(2, bitmapData.Count);
+
+            while (bitmapData.Count % 4 != 0)
+                bitmapData.Add(0);
+
+            File.WriteAllBytes(fileName, bitmapData.ToArray());
+        }
+    }
+
+    internal class ColorEquality : IEqualityComparer<Color>
+    {
+        public bool Equals(Color x, Color y)
+        {
+            return x.ToArgb() == y.ToArgb();
+        }
+
+        public int GetHashCode(Color obj)
+        {
+            return obj.ToArgb();
+        }
+    }
+
+    internal static class ListExtensions
+    {
+        public static void AddUShort(this List<byte> list, ushort number)
+        {
+            list.Add((byte)number);
+            list.Add((byte)(number >> 8));
+        }
+
+        public static void AddUInt(this List<byte> list, uint number)
+        {
+            list.Add((byte)number);
+            list.Add((byte)(number >> 8));
+            list.Add((byte)(number >> 16));
+            list.Add((byte)(number >> 24));
+        }
+
+        public static void AddInt(this List<byte> list, int number)
+        {
+            AddUInt(list, (uint)number);
+        }
+
+        public static void AddColor(this List<byte> list, Color color, bool alpha)
+        {
+            list.Add(color.B);
+            list.Add(color.G);
+            list.Add(color.R);
+            list.Add(alpha ? color.A : (byte)0);
+        }
+
+        public static void SetUInt(this List<byte> list, int index, uint number)
+        {
+            list[index + 0] = (byte)number;
+            list[index + 1] = (byte)(number >> 8);
+            list[index + 2] = (byte)(number >> 16);
+            list[index + 3] = (byte)(number >> 24);
+        }
+
+        public static void SetInt(this List<byte> list, int index, int number)
+        {
+            SetUInt(list, index, (uint)number);
+        }
+
+    }
+}
